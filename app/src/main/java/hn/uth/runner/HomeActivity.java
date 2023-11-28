64577f48d83baed7557e86345cf72f2b61540663
webiopi <33105511+webiopi@users.noreply.github.com>
2023-11-27 19:22:23 -0600

    Se agregaron los cambios de Ivan, en el Main Activity, activity_recuperacion, clase Recuperacion, drawable imagen web, strings para recuperacion.
    Por mi parte se termino de complementar la pantalla de registro del usuario que es en la Clase Home y Activity home. Se agrego campo de Nombre y Pais y este se guarda en BD en Firebase como lo solicita el proyecto. Queda pendiente nada mas captura y guardado de imagen en BD.

--- app/src/main/java/hn/uth/runner/HomeActivity.java
+++ app/src/main/java/hn/uth/runner/HomeActivity.java
@@ -1,14 +1,24 @@
 package hn.uth.runner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 class ProviderType {
     public static final String BASICA = "BASICA";
@@ -23,17 +33,35 @@ class ProviderType {
 }
 
 public class HomeActivity extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    ImageView viewContacto;
+
     TextView viewCorreo;
     TextView viewProveedor;
+
+    EditText txtNombre;
+    EditText txtPais;
+
+    Button btGuardar;
+    Button btFoto;
     Button btCerrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        viewContacto= (ImageView) findViewById(R.id.viewContacto);
 
         viewCorreo= (TextView) findViewById(R.id.viewCorreo);
         viewProveedor= (TextView) findViewById(R.id.viewProveedor);
+
+        txtNombre= (EditText) findViewById(R.id.txtNombre);
+        txtPais= (EditText) findViewById(R.id.txtPais);
+
+        btGuardar= (Button) findViewById(R.id.btGuardar);
+        btFoto= (Button) findViewById(R.id.btFoto);
         btCerrar= (Button) findViewById(R.id.btCerrar);
 
         //Llamar a Setup
@@ -51,6 +79,53 @@ public class HomeActivity extends AppCompatActivity {
         // Asignar el valor del proveedor al TextView viewProveedor
         viewProveedor.setText(proveedor);
 
+        //Empieza Codigo de los Eventos de los Botones
+        btGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nombre = txtNombre.getText().toString().trim();
+                String pais = txtPais.getText().toString().trim();
+
+                if (nombre.isEmpty() || pais.isEmpty()) {
+                    // Mostrar mensaje de error si los campos están vacíos
+                    Toast.makeText(getApplicationContext(), "Por favor, completa todos los campos", Toast.LENGTH_SHORT).show();
+                } else {
+                    Map<String, Object> data = new HashMap<>();
+                    data.put("proveedor", proveedor);
+                    data.put("nombre", nombre);
+                    data.put("pais", pais);
+
+                    db.collection("usuarios").document(correo).set(data)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    // Operación exitosa
+                                    Toast.makeText(getApplicationContext(), "Datos guardados con éxito", Toast.LENGTH_SHORT).show();
+
+                                    // Limpiar los campos después de guardar exitosamente
+                                    txtNombre.setText("");
+                                    txtPais.setText("");
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    // Error al guardar
+                                    Toast.makeText(getApplicationContext(), "Error al guardar datos", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                }
+            }
+        });
+
+        btFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
         btCerrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,5 +133,6 @@ public class HomeActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
     }
 }
--- app/src/main/java/hn/uth/runner/MainActivity.java
+++ app/src/main/java/hn/uth/runner/MainActivity.java
@@ -5,20 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAnalytics mFirebaseAnalytics;
+    private FirebaseAuth mAuth;
 
+    private static final String TAG = "MainActivity";
     EditText Correo;
     EditText Contraseña;
 
@@ -27,9 +30,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
 
         Correo= (EditText) findViewById(R.id.txtCorreo);
         Contraseña= (EditText) findViewById(R.id.txtContraseña);
@@ -38,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         Registro= (Button) findViewById(R.id.btRegistro);
 
 
+        mAuth = FirebaseAuth.getInstance();
         Registro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,6 +59,22 @@ public class MainActivity extends AppCompatActivity {
                                 ProviderType proveedor = new ProviderType("BASICA");
                                 MostrarHome(Correo.getText().toString(), proveedor);
                                 Toast.makeText(getApplicationContext(), "El registro se ha creado con exito", Toast.LENGTH_LONG).show();
+                                final FirebaseUser user = mAuth.getCurrentUser();
+                                user.sendEmailVerification()
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    Toast.makeText(MainActivity.this, "Correo De Verificacion Enviado A: " + user.getEmail(), Toast.LENGTH_SHORT).show();
+                                                    ProviderType proveedor = new ProviderType("BASICA");
+                                                    //MostrarHome(Correo.getText().toString(), proveedor);
+                                                    Toast.makeText(getApplicationContext(), "El registro se ha creado con exito", Toast.LENGTH_LONG).show();
+                                                } else {
+                                                    Log.e(TAG, "sendEmailVerification", task.getException());
+                                                    Toast.makeText(MainActivity.this, "Error Al Enviar La Verificacion De Correo.", Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        });
                             }else {
                                 Toast.makeText(getApplicationContext(), "Se ha producido un error al REGISTRAR al usuario", Toast.LENGTH_LONG).show();
                             }
@@ -61,7 +83,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
         Sesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -74,10 +95,15 @@ public class MainActivity extends AppCompatActivity {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()){
-                                ProviderType proveedor = new ProviderType("BASICA");
-                                MostrarHome(Correo.getText().toString(), proveedor);
-                                Toast.makeText(getApplicationContext(), "Bienvenido", Toast.LENGTH_LONG).show();
-                            }else {
+                                if (mAuth.getCurrentUser().isEmailVerified()){
+                                    ProviderType proveedor = new ProviderType("BASICA");
+                                    MostrarHome(Correo.getText().toString(), proveedor);
+                                    Toast.makeText(getApplicationContext(), "Bienvenido", Toast.LENGTH_LONG).show();
+                                }else{
+                                    Toast.makeText(getApplicationContext(), "Porfavor Verifique Su Correo Electronico", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                            else {
                                 Toast.makeText(getApplicationContext(), "Se ha producido un error al AUTENTICAR al usuario", Toast.LENGTH_LONG).show();
                             }
                         }
@@ -86,7 +112,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
     public void MostrarHome(String correo, ProviderType proveedor) {
         Intent intent = new Intent(this, HomeActivity.class);
         intent.putExtra("correo", correo);
